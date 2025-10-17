ba466324dae8325c7d2ff354bbbe753af4528952
Dinzann <yaohongyu3262@gmail.com>
2025-10-17 18:35:37 +0800

    新的fish配置

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,34 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive # Commands to run in interactive sessions can go here
+
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    starship init fish | source
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+
+    # Aliases
+    alias pamcan pacman
+    alias ls 'eza --icons'
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias q 'qs -c ii'
+
+end
+
 starship init fish | source
 
 #运行fastfetch
-fastfetch
+#fastfetch
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
@@ -14,3 +41,11 @@ function y
     end
     rm -f -- "$tmp"
 end
+
+abbr -a f fastfetch
+
+# Fcitx 输入法环境变量
+set -gx GTK_IM_MODULE fcitx
+set -gx QT_IM_MODULE fcitx
+set -gx XMODIFIERS @im=fcitx
+set -gx SDL_IM_MODULE fcitx
