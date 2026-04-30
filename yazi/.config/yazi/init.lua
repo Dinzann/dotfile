33059ed95c12f75618000ce71c62985acc1e303f
Dinzann <yaohongyu3262@gmail.com>
2026-04-30 19:07:59 +0800

    change theme

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -2,9 +2,9 @@ require("git"):setup {
   order = 1500,
 }
 
-require("yaziline"):setup()
-
 require("full-border"):setup {
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 }
+
+require("simple-status"):setup()
