1907ee40c75e31a271735e8f3dc6d7dfc6ebb33e
Dinzann <yaohongyu3262@gmail.com>
2026-07-25 20:24:21 +0800

    add cava config in macos

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,10 +1,10 @@
-require("git"):setup {
-  order = 1500,
-}
+require("git"):setup({
+	order = 1500,
+})
 
-require("full-border"):setup {
+require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
-}
+})
 
 require("simple-status"):setup()
